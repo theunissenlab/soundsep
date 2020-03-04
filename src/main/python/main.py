40d6118868514fe75026aa2351866548f7ff58fe
Kevin Yu <thekevinyu@gmail.com>
2020-03-04 10:17:36 -0800

    Add pyqtgraph to requirements and make gui run

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,6 +1,9 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from PyQt5.QtCore import QObject
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import QtWidgets as widgets
+
+import pyqtgraph as pg
 
 import sys
 
@@ -24,6 +27,33 @@ class App(widgets.QMainWindow):
         self.main_view = SpectrogramVisualizer(self)
         self.setCentralWidget(self.main_view)
         self.show()
+
+
+class SpectrogramVisualizer(widgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.init_ui()
+
+    def init_ui(self):
+        self.groupbox1 = widgets.QGroupBox("Scatter 2D")
+
+        self.plot = pg.PlotWidget()
+
+        pen = pg.mkPen((200, 200, 250, 127))
+
+        self.scatter = pg.ScatterPlotItem(pen=pen, symbol="o", size=1)
+
+        self.plot.setLimits(xMin=None, xMax=None, yMin=None, yMax=None)
+
+        self.scatter.hoverEvent = None
+        self.scatter.mouseClickEvent = None
+
+        self.plot.addItem(self.scatter)
+
+        layout = widgets.QVBoxLayout()
+        layout.addWidget(self.plot)
+        layout.addStretch(1)
+        self.groupbox1.setLayout(layout)
 
     
 if __name__ == '__main__':
